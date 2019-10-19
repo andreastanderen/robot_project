7339f5cb69a70aa9d2d0e3f18934c9f3e1693637
andreastanderen <andrea_standeren@hotmail.com>
2019-10-19 22:48:42 +0200

    add class BBCON

--- robot_project/robot.py
+++ robot_project/robot.py
@@ -17,3 +17,36 @@ class BBCON:
         # mulig vi må lage en ny behavior-instans her...
         self.behaviors.append(behavior)
 
+
+    def add_sensob(self, sensob):
+        self.sensobs.append(sensob)
+
+
+    def activate_behavior(self, behavior):
+        """ if behavior is an existing behavior, add to
+            active_behaviors """
+        if behavior not in self.behaviors:
+            return
+        self.active_behaviors.append(behavior)
+
+    def deactivate_behavior(self, behavior):
+        """ if behavior is an existing behavior, remove
+            from active_behaviors """
+        if behavior not in self.behaviors:
+            return
+        self.active_behaviors.remove(behavior)
+
+    def run_one_timestep(self):
+        """ this is the core of the BBCON.
+            update all sensobs and behaviors """
+        # self.arbit..choose_act... chooses a winning behavior and returns
+        # the winning behavors motor recommendations and halt_request
+        motor_recs, halt_request = self.arbitrator.choose_action()
+        self.motobs.update(motor_recs) # update motobs which then updates all motors
+        for sensob in self.sensobs:
+            sensob.reset() # may need to reset associated sensors...
+
+
+
+
+
