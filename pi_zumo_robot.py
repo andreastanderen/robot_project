4db2bd4e424d03324459ebfd6a4559d4a57f7f1c
aleksols <aleko@stud.ntnu.no>
2019-10-31 14:22:46 +0100

    Update

---
+++ pi_zumo_robot.py
@@ -0,0 +1,28 @@
+import robot
+from camera import Camera
+from irproximity_sensor import IRProximitySensor
+from reflectance_sensors import ReflectanceSensors
+from zumo_button import ZumoButton
+
+
+class PiZumoRobot:
+
+    def __init__(self):
+        self.controller = robot.BBCON()
+        self.controller.arbitrator = robot.arbitrator(self)
+
+        self.controller.motobs = [robot.Motob]
+
+        self.reflect_sensob = robot.Sensob([ReflectanceSensors(auto_calibrate=True)])
+        self.cam_sensob = robot.Sensob([Camera()])
+
+
+    def run(self):
+        button = ZumoButton()
+        button.wait_for_press()
+        while True:
+            self.controller.run_one_timestep()
+
+if __name__ == '__main__':
+    zumo = PiZumoRobot()
+    zumo.run()
--- robot.py
+++ robot.py
@@ -1,3 +1,9 @@
+import sys
+import time
+
+import motors
+
+
 class BBCON:
 
     def __init__(self):  # arbit
@@ -6,7 +12,7 @@ class BBCON:
         self.sensobs = []
         self.motobs = []
         self.arbitrator = None  # arbit
-        self.current_timestep = None
+        self.current_timestep = 0
         self.inactive_behavior = []
         self.currently_controlled = None
 
@@ -24,6 +30,7 @@ class BBCON:
         if behavior not in self.behaviors:
             return
         self.active_behaviors.append(behavior)
+        behavior.active_flag = True
 
     def deactivate_behavior(self, behavior):
         """ if behavior is an existing behavior, remove
@@ -31,22 +38,33 @@ class BBCON:
         if behavior not in self.active_behaviors:
             return
         self.active_behaviors.remove(behavior)
+        behavior.active_flag = False
 
     def run_one_timestep(self):
         """ this is the core of the BBCON.
             update all sensobs and behaviors """
         # self.arbit..choose_act... chooses a winning behavior and returns
         # the winning behavors motor recommendations and halt_request
+        [sensob.update() for sensob in self.sensobs]
+        [behavior.update() for behavior in self.behaviors]
         motor_recs, halt_request = self.arbitrator.choose_action()
-        self.motobs.update(motor_recs)  # update motobs which then updates all motors
+        if halt_request:
+            sys.exit()
+        for i, rec in enumerate(motor_recs):
+            self.motobs[i].update(rec)
+
+        # self.motobs.update(motor_recs)  # update motobs which then updates all motors
         for sensob in self.sensobs:
             sensob.reset()  # may need to reset associated sensors...
+        time.sleep(0.5)
+        [sensob.reset() for sensob in self.sensobs]
+        self.current_timestep += 1
 
 
 class Sensob:
 
-    def __init__(self):
-        self.sensors = []
+    def __init__(self, sensors=[]):
+        self.sensors = sensors
         self.values = []
 
     def update(self):
@@ -54,13 +72,58 @@ class Sensob:
             self.values[i] = sensor.get.value()
 
 
-class SensorWrapper:
+class Motob:
+
     def __init__(self):
+        self.motors = [motors.Motors()]
+        self.value = None
+
+    def update(self, recommendation):
+        self.value = recommendation
+
+    def operationalize(self):
         pass
+
+
+class Behavior:
+    def __init__(self, controller: BBCON, priority):
+        self.controller = controller
+        self.sensobs = []
+        self.motor_recommendations = []
+        self.active_flag = False
+        self.halt_request = False
+        self.priority = priority
+        self.match_degree = 0
+        self.weight = 0
+
+    def consider_deactivation(self):
+        if not self.active_flag:
+            return
+
+    def consider_activation(self):
+        if self.active_flag:
+            return
 
     def update(self):
+
+        self.sense_and_act()
+
+    def sense_and_act(self):
         pass
 
-    def get_value(self):
-        pass
+
+class Arbitrator:
+
+    def __init__(self, controller: BBCON):
+        self.controller = controller
+
+    def choose_action(self):
+        action = max(self.controller.active_behaviors, key=lambda b: b.weight)
+        return action.motor_recommendations, action.halt_request
+
+
+if __name__ == '__main__':
+    controller = BBCON()
+    arbitrator = Arbitrator(controller)
+    controller.arbitrator = arbitrator
 
