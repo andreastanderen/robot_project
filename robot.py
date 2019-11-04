f79c1b89095af6cdef74424e7a628e4f8d18743b
aleksols <aleko@stud.ntnu.no>
2019-11-04 12:31:03 +0100

    Add motob function

--- robot.py
+++ robot.py
@@ -59,7 +59,7 @@ class BBCON:
             sys.exit()
 
         self.motobs[0].update(motor_recs)
-
+        self.motobs[0].operationalize()
         # self.motobs.update(motor_recs)  # update motobs which then updates all motors
         time.sleep(0.5)
         [sensob.reset() for sensob in self.sensobs]
